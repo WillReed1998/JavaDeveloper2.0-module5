6ffd79a04f942d77b4c4cc3e32b850be934b362e
Nikita Kovalov <nik.kovalev1760@gmail.com>
2023-08-22 06:14:53 -0400

    1.1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,5 @@
 package org.example;
 
-
-import static org.example.FibonacciDP.fibonacciDP;
-import static org.example.FibonacciRecursive.fibonacciRecursive;
-
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
         int n = 10;
